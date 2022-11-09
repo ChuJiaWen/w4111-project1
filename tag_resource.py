05c4e49caa318496276d9c7d3534803df02b1caf
Jiawen Chu <sab1025@bu.edu>
2022-11-09 15:47:55 -0500

    "Completed Go-into-Albums & Upload-a-photo & See-spefic-album-photos function"

---
+++ tag_resource.py
@@ -0,0 +1,25 @@
+import flask_login
+from flask import Flask, request, render_template, g, redirect, Response, url_for
+
+import album_resource,user_resource
+from datetime import datetime
+
+def getAllTags():
+    result = g.conn.execute("SELECT tag_name as count FROM Tags ORDER BY tag_name").fetchall()
+    return result  # NOTE list of tuples, [(tag_name,), ...]
+
+# def getTopMyTags(uid):
+#     result = g.conn.execute("SELECT description, count(pid) as count FROM Tags_Associates WHERE uid='{0}' GROUP BY description ORDER BY count DESC LIMIT 5".format(uid)).fetchall()
+#     return result
+
+# def getMyTags(uid):
+#     result = g.conn.execute("SELECT description, pid FROM Associates WHERE  uid = '{0}'".format(uid)).fetchall()
+#     return result  # NOTE list of tuples, [(description, pid), ...]
+
+# def getTagsPhotosAll(description):
+#     result = g.conn.execute("SELECT description, pid FROM Tags_Associates WHERE description = '{0}'".format(description)).fetchall()
+#     return result  # NOTE list of tuples, [(description, pid), ...]
+
+# def getTagsPhotosMy(description,uid):
+#     result = g.conn.execute("SELECT description, pid FROM Tags_Associates WHERE description = '{0}' AND uid = '{1}'".format(description,uid)).fetchall()
+#     return result  # NOTE list of tuples, [(description, pid), ...]
